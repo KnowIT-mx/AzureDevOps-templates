5c7f788421112925b1e9c4a3f2eba721f89241da
José Ramón Aguilar <jraguilar@knowit.mx>
2020-10-24 14:47:16 -0500

    Agrega soporte para Deployment Slots en WebApps

--- deploy/webapp.ps1
+++ deploy/webapp.ps1
@@ -2,10 +2,14 @@
 az group create --name $env:AZURE_RESOURCEGROUP --location $env:AZURE_LOCATION
 az configure --defaults group=$env:AZURE_RESOURCEGROUP
 
-"[command][+] Application Service Plan [$env:APPSERVICEPLAN]..."
-az appservice plan create --name $env:APPSERVICEPLAN --is-linux --sku B1
+"[command][+] Application Service Plan [$env:APPSERVICE_NAME]..."
+az appservice plan create --name $env:APPSERVICE_NAME --is-linux --sku $env:APPSERVICE_TIER
 
 "[command][+] WebApp [$env:WEBAPP]..."
-az webapp create --name $env:WEBAPP --plan $env:APPSERVICEPLAN --runtime """DOTNETCORE|$env:NETCORE_VERSION"""
-az webapp identity assign --name $env:WEBAPP --resource-group $env:AZURE_RESOURCEGROUP
+az webapp create --name $env:WEBAPP --plan $env:APPSERVICE_NAME --runtime """DOTNETCORE|$env:NETCORE_VERSION"""
+if($env:WEBAPP_SLOT) {
+    az webapp deployment slot create --name $env:WEBAPP --slot $env:WEBAPP_SLOT
+}
 az webapp config connection-string set --name $env:WEBAPP --connection-string-type SQLAzure --settings default="""$env:CONN_STRING"""
+$identity = az webapp identity assign --name $env:WEBAPP --resource-group $env:AZURE_RESOURCEGROUP --query principalId -o tsv
+az keyvault set-policy -n $env:AZURE_RESOURCEGROUP --secret-permissions get list --object-id $identity
