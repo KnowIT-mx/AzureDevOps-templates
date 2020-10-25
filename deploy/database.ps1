9bc1bed5e1c552c3ca6407b8808ace65a65dad18
José Ramón Aguilar <jraguilar@knowit.mx>
2020-10-24 21:55:18 -0500

    Agrega stage independiente para Deploy de SQL

--- deploy/database.ps1
+++ deploy/database.ps1
@@ -2,12 +2,14 @@
 az group create --name $env:AZURE_RESOURCEGROUP --location $env:AZURE_LOCATION
 az configure --defaults group=$env:AZURE_RESOURCEGROUP
 
-"[command][+] Crea/configura AzureSQL Server [$env:DBSERVER]..."
-az sql server create --name $env:DBSERVER --admin-user dios --admin-password $env:DBPASSWORD
-az sql server firewall-rule create --server $env:DBSERVER -n AllowAllAzureServiceIps --start-ip-address 0.0.0.0 --end-ip-address 0.0.0.0
+#"[command][+] Crea/configura AzureSQL Server [$env:DBSERVER]..."
+#az sql server create --name $env:DBSERVER --admin-user dios --admin-password $env:DBPASSWORD
+#az sql server firewall-rule create --server $env:DBSERVER -n AllowAllAzureServiceIps --start-ip-address 0.0.0.0 --end-ip-address 0.0.0.0
 
+"[command][*] Validando si existe la base de datos..."
+$dbName = if($env:DEPLOYMENTSLOT) { "${env:DBNAME}-${env:DEPLOYMENTSLOT}" } else { $env:DBNAME }
 $db = az sql db list --server $env:DBSERVER | ConvertFrom-Json
-if($env:DBNAME -notin $db.name) {
+if($dbName -notin $db.name) {
     "[command][+] Crea nueva base de datos..."
-    az sql db create --name $env:DBNAME --server $env:DBSERVER --service-objective Basic
+    az sql db create --name $dbName --server $env:DBSERVER --compute-model Serverless --capacity 1 -e GeneralPurpose -f Gen5
 }
--- deploy/webapp.ps1
+++ deploy/webapp.ps1
@@ -1,4 +1,5 @@
 "[command][+] Resource Group [$env:AZURE_RESOURCEGROUP]..."
+$env:AZURE_CORE_OUTPUT = 'table'
 az group create --name $env:AZURE_RESOURCEGROUP --location $env:AZURE_LOCATION
 az configure --defaults group=$env:AZURE_RESOURCEGROUP
 
@@ -8,8 +9,14 @@ az appservice plan create --name $env:APPSERVICE_NAME --is-linux --sku $env:APPS
 "[command][+] WebApp [$env:WEBAPP]..."
 az webapp create --name $env:WEBAPP --plan $env:APPSERVICE_NAME --runtime """DOTNETCORE|$env:NETCORE_VERSION"""
 if($env:WEBAPP_SLOT) {
+    "[command]  [-] WebApp Slot [$env:WEBAPP_SLOT]"
     az webapp deployment slot create --name $env:WEBAPP --slot $env:WEBAPP_SLOT
+    az webapp config connection-string set --name $env:WEBAPP --slot $env:WEBAPP_SLOT --connection-string-type SQLAzure --slot-settings default="""$env:NETCORE_CONNECTIONSTRING""" -o jsonc
+    $identity = az webapp identity assign --name $env:WEBAPP --slot $env:WEBAPP_SLOT --query principalId -o tsv
 }
-az webapp config connection-string set --name $env:WEBAPP --connection-string-type SQLAzure --settings default="""$env:CONN_STRING"""
-$identity = az webapp identity assign --name $env:WEBAPP --resource-group $env:AZURE_RESOURCEGROUP --query principalId -o tsv
+else {
+    az webapp config connection-string set --name $env:WEBAPP --connection-string-type SQLAzure --settings default="""$env:NETCORE_CONNECTIONSTRING""" -o jsonc
+    $identity = az webapp identity assign --name $env:WEBAPP --query principalId -o tsv
+}
+"[command]  [-] Concede acceso a KeyVault [$env:AZURE_RESOURCEGROUP]"
 az keyvault set-policy -n $env:AZURE_RESOURCEGROUP --secret-permissions get list --object-id $identity
