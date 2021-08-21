329d23ff694323294d645244ca950b05ae738a7e
José Ramón Aguilar <jraguilar@knowit.mx>
2021-08-21 00:32:36 -0500

    Separa stage para deploy de DB en template independiente

--- deploy/database.ps1
+++ deploy/database.ps1
@@ -1,11 +1,12 @@
 "[command][+] Resource Group [$env:AZURE_RESOURCEGROUP]..."
-az group create --name $env:AZURE_RESOURCEGROUP --location $env:AZURE_LOCATION
+#az group create --name $env:AZURE_RESOURCEGROUP --location $env:AZURE_LOCATION
 az configure --defaults group=$env:AZURE_RESOURCEGROUP
 
-"[command][+] Crea/configura AzureSQL Server [$env:DBSERVER]..."
-az sql server create --name $env:DBSERVER --admin-user dios --admin-password $env:DBPASSWORD
-az sql server firewall-rule create --server $env:DBSERVER -n AllowAllAzureServiceIps --start-ip-address 0.0.0.0 --end-ip-address 0.0.0.0
+#"[command][+] Crea/configura AzureSQL Server [$env:DBSERVER]..."
+#az sql server create --name $env:DBSERVER --admin-user dios --admin-password $env:DBPASSWORD
+#az sql server firewall-rule create --server $env:DBSERVER -n AllowAllAzureServiceIps --start-ip-address 0.0.0.0 --end-ip-address 0.0.0.0
 
+"[command][*] Validando si existe la base de datos..."
 $db = az sql db list --server $env:DBSERVER | ConvertFrom-Json
 if($env:DBNAME -notin $db.name) {
     "[command][+] Crea nueva base de datos..."
