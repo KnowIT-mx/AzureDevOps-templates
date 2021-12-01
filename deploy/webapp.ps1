b78da5454be6ea824c01379858f169a13a59b8d3
José Ramón Aguilar <jraguilar@knowit.mx>
2021-11-30 22:47:50 -0600

    deploy

--- deploy/webapp.ps1
+++ deploy/webapp.ps1
@@ -6,6 +6,6 @@ az configure --defaults group=$env:AZURE_RESOURCEGROUP
 az appservice plan create --name $env:WEBAPP_SERVICEPLAN --is-linux --sku B1
 
 "[command][+] WebApp [$env:WEBAPP_NAME]..."
-az webapp create --name $env:WEBAPP_NAME --plan $env:WEBAPP_SERVICEPLAN --runtime """$env:RUNTIME"""
+az webapp create --name $env:WEBAPP_NAME --plan $env:WEBAPP_SERVICEPLAN --runtime """$env:WEBAPP_RUNTIME"""
 az webapp identity assign --name $env:WEBAPP_NAME --resource-group $env:AZURE_RESOURCEGROUP
 #az webapp config connection-string set --name $env:WEBAPP_NAME --connection-string-type SQLAzure --settings default="""$env:CONN_STRING"""
